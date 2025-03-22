3cc7e3e531663c7bb7d291815195ce97a7fd9b8b
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-03-22 13:27:49 +0100

    dev

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,7 +3,6 @@ use Contao\CoreBundle\ContaoCoreBundle;
 
 $GLOBALS['ISO_PRODUCT']['IsoMasonry'] = 'Bits\IsoMasonry\Model\Product\IsoMasonryProduct';
 
-
 return [
    
     ContaoCoreBundle::class => ['all' => true],
