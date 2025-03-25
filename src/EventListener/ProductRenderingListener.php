e2f6a10938f2b94e24e94fb0b6a9f32073ef8bd2
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-03-25 09:41:27 +0100

    dev

--- src/EventListener/ProductRenderingListener.php
+++ src/EventListener/ProductRenderingListener.php
@@ -2,6 +2,7 @@
 namespace Bits\IsoMasonryBundle\EventListener;
 
 use Contao\System;
+use Isotope\Model\AttributeOption;
 use Bits\IsoMasonryBundle\Service\ImageResizer;
 
 
@@ -10,12 +11,51 @@ class ProductRenderingListener
      
     public function alterProductRendering($objTemplate, $objProduct, $arrConfig)
     {
-        $imageResizer = new ImageResizer();
+        
         $arrImages = deserialize($objProduct->__get('images'));
         $isotopeSubFolder = mb_substr($arrImages[0]['src'],0,1);
-        $objTemplate->src_preview = $imageResizer->resizeAndCacheImage("/isotope/".$isotopeSubFolder."/".$arrImages[0]['src'], 300,null);
-        $objTemplate->src_fullsize = $imageResizer->resizeAndCacheImage("/isotope/".$isotopeSubFolder."/".$arrImages[0]['src'], 1480,900);
-
+        $imageResizer = new ImageResizer();
         
+        if($objProduct->getType()->__get('name') == 'Tattoo'){
+                
+                $arrAttributeMotiv = explode(',',$objProduct->__get('motiv'));
+                $arrMotiv = [];
+                foreach($arrAttributeMotiv as $key => $Motiv){
+                    $arrMotiv[$key]['label'] = AttributeOption::findPublishedByIds([$Motiv],[])->__get('label');
+                    $arrMotiv[$key]['alias'] = $this->generateAlias($arrMotiv[$key]['label']);
+                    }
+                    
+                //$objTemplate->setData(['arr_motiv'=>$arrMotiv]);
+                $objProduct->__set('arr_motiv',$arrMotiv);
+                //var_dump($objTemplate->product);
+                //$objTemplate->product = $objProduct;
+                $objTemplate->arr_motiv = $arrMotiv;
+                
+                
+                $objTemplate->src_preview = $imageResizer->resizeAndCacheImage("/isotope/".$isotopeSubFolder."/".$arrImages[0]['src'], 300,null);
+                $objTemplate->src_fullsize = $imageResizer->resizeAndCacheImage("/isotope/".$isotopeSubFolder."/".$arrImages[0]['src'], 1480,900);
+        }elseif($objProduct->getType()->__get('name') == 'Amazon'){
+            
+                $objTemplate->src = $imageResizer->resizeAndCacheImage("/isotope/".$isotopeSubFolder."/".$arrImages[0]['src'], 400,400);
+       
+            
+        }
+        
+        
+    }
+    
+    private function generateAlias(string $input): string
+    {
+        $alias = strip_tags($input);
+
+        $transliteration = [
+            'ä' => 'ae', 'ö' => 'oe', 'ü' => 'ue', 'ß' => 'ss',
+            'Ä' => 'Ae', 'Ö' => 'Oe', 'Ü' => 'Ue',
+        ];
+        $alias = strtr($alias, $transliteration);
+
+        $alias = preg_replace('/[^a-zA-Z0-9\-]+/', '-', $alias);
+        $alias = trim($alias, '-');
+        return strtolower($alias);
     }
 }
